a6fe1972f2f8f1ee5d0f644eb85ca27d65186de9
Miral <miralmodi09@gmail.com>
2020-05-12 23:48:07 +0530

    chnages message.

--- src/main/java/com/techprimers/security/jwtsecurity/controller/HelloJava.java
+++ src/main/java/com/techprimers/security/jwtsecurity/controller/HelloJava.java
@@ -3,9 +3,12 @@ package com.techprimers.security.jwtsecurity.controller;
 public class HelloJava {
 
     public void printHello(){
-        System.out.println("hello world");
+        System.out.println("hello India");
     }
 
+    public void print(){
+        System.out.println("Data changes");
+    }
     public static void main(String[] args){
         HelloJava helloJava = new HelloJava();
         helloJava.printHello();
