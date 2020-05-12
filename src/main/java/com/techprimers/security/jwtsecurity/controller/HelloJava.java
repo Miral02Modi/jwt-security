27b9688bc8106a92b97005c658c588b7ec5f3136
Miral <miralmodi09@gmail.com>
2020-05-12 23:35:08 +0530

    create hello class to print hello msg.

---
+++ src/main/java/com/techprimers/security/jwtsecurity/controller/HelloJava.java
@@ -0,0 +1,13 @@
+package com.techprimers.security.jwtsecurity.controller;
+
+public class HelloJava {
+
+    public void printHello(){
+        System.out.println("Hellow");
+    }
+
+    public static void main(String[] args){
+        HelloJava helloJava = new HelloJava();
+        helloJava.printHello();
+    }
+}
